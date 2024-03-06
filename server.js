a988d783539cc38102abd73f6b8c5120eaaa4cc8
Vlad <vlad@Vlads-MacBook-Pro.local>
2024-03-06 10:32:31 +0200

    improved pagination

--- server.js
+++ server.js
@@ -9,17 +9,40 @@ const API_KEY =
   'sk_prod_TfMbARhdgues5AuIosvvdAC9WsA5kXiZlW8HZPaRDlIbCpSpLsXBeZO7dCVZQwHAY3P4VSBPiiC33poZ1tdUj2ljOzdTCCOSpUZ_3912';
 
 app.get('/:formId/filteredResponses', async (req, res) => {
-  const querys = req.query;
-  try {
-    const formId = req.params.formId;
-    const API = `https://api.fillout.com/v1/api/forms/${formId}/submissions`;
-    const { data } = await axios.get(API, {
-      headers: {
-        Authorization: `Bearer ${API_KEY}`,
-      },
-      params: querys,
-    });
+  const { offset, ...querys } = req.query;
+  const formId = req.params.formId;
 
+  try {
+    async function fetchAllData() {
+      const API = `https://api.fillout.com/v1/api/forms/${formId}/submissions`;
+      const { data } = await axios.get(API, {
+        headers: {
+          Authorization: `Bearer ${API_KEY}`,
+        },
+        params: querys,
+      });
+      const result = {
+        responses: data.responses,
+        totalResponses: data.totalResponses,
+        pageCount: data.pageCount,
+      };
+
+      if (data.pageCount > 1) {
+        for (let i = 1; i < data.pageCount; i++) {
+          const { data } = await axios.get(API, {
+            headers: {
+              Authorization: `Bearer ${API_KEY}`,
+            },
+            params: { offset: querys.limit * i, limit: querys.limit },
+          });
+          result.responses.push(...data.responses);
+        }
+        return result;
+      }
+      return result;
+    }
+
+    const data = await fetchAllData();
     const filters = JSON.parse(req.query?.filters);
 
     const newData = data.responses.filter((submission) => {
@@ -43,15 +66,26 @@ app.get('/:formId/filteredResponses', async (req, res) => {
         }
       });
     });
-    const response = {
-      responses: newData,
-      totalResponses: newData.length,
-      //If you need to implement pagination with inner filtered item then we need to add new query like this:
-      //pageCount: querys?.innerFiler ?  Math.ceil(newData.length /  querys?.innerFiler) : 1
-      // I set 1 becose limit is maximum 150, we cant get all together
-      pageCount: 1,
-    };
-    res.json(response);
+
+    if (querys?.limit) {
+      const response = {
+        responses: querys?.offset
+          ? newData.slice(querys.offset, querys.offset + querys.limit)
+          : newData.slice(0, querys.limit),
+        totalResponses: newData.length,
+        pageCount: querys?.limit
+          ? Math.ceil(newData.length / querys?.limit)
+          : Math.ceil(newData.length / 150),
+      };
+      res.json(response);
+    } else {
+      const response = {
+        responses: newData,
+        totalResponses: newData.length,
+        pageCount: 1,
+      };
+      res.json(response);
+    }
   } catch (error) {
     console.error('Error:', error);
     res.status(500).json({ error: 'Server Error' });
